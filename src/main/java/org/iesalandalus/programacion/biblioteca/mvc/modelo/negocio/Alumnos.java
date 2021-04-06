e37baa79c8f6864af19f8aa68f6062a4ae5a53e5
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-04-06 12:09:57 +0200

    Arreglo de Alumnos
    Añado comentarios que faltaban y reformateo un poco el código

--- src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/Alumnos.java
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/modelo/negocio/Alumnos.java
@@ -9,7 +9,7 @@ import javax.naming.OperationNotSupportedException;
 import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Alumno;
 
 public class Alumnos {
-	
+
 	private List<Alumno> coleccionAlumnos;
 
 	/** Constructor por defecto de la clase Alumnos */
@@ -21,17 +21,19 @@ public class Alumnos {
 	public List<Alumno> get() {
 		List<Alumno> copiaAlumnos = copiaProfundaAlumnos();
 		copiaAlumnos.sort(Comparator.comparing(Alumno::getNombre));
-		
+
 		return copiaAlumnos;
 	}
 
-	/** Método para realizar copia profunda con referencias distintas de los alumnos */
+	/**
+	 * Método para realizar copia profunda con referencias distintas de los alumnos
+	 */
 	private List<Alumno> copiaProfundaAlumnos() {
 		List<Alumno> copiaAlumnos = new ArrayList<>();
-		
+
 		for (Alumno alumno : coleccionAlumnos)
 			copiaAlumnos.add(new Alumno(alumno));
-		
+
 		return copiaAlumnos;
 	}
 
@@ -39,34 +41,36 @@ public class Alumnos {
 		return coleccionAlumnos.size();
 	}
 
+	/** Método para insertar un alumno nuevo */
 	public void insertar(Alumno alumno) throws OperationNotSupportedException {
 		if (alumno == null)
 			throw new NullPointerException("ERROR: No se puede insertar un alumno nulo.");
 
 		if (coleccionAlumnos.contains(alumno))
 			throw new OperationNotSupportedException("ERROR: Ya existe un alumno con ese correo.");
-		
-		coleccionAlumnos.add(new Alumno(alumno));
 
+		coleccionAlumnos.add(new Alumno(alumno));
 	}
 
+	/** Método para buscar un alumno */
 	public Alumno buscar(Alumno alumno) {
 		if (alumno == null)
 			throw new IllegalArgumentException("ERROR: No se puede buscar un alumno nulo.");
-		
+
 		int indice = coleccionAlumnos.indexOf(alumno);
 		alumno = (indice == -1) ? null : new Alumno(coleccionAlumnos.get(indice));
-		
+
 		return alumno;
 	}
 
+	/** Método para borrar un alumno */
 	public void borrar(Alumno alumno) throws OperationNotSupportedException {
 		if (alumno == null)
 			throw new IllegalArgumentException("ERROR: No se puede borrar un alumno nulo.");
 
 		if (!coleccionAlumnos.contains(alumno))
 			throw new OperationNotSupportedException("ERROR: No existe ningún alumno con ese correo.");
-		
+
 		coleccionAlumnos.remove(alumno);
 	}
 
